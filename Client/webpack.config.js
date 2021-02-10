bb26002af7785c9af130c662f72e8ed9e3f580b8
Torphexi <thiel1997@gmail.com>
2021-02-10 15:07:18 +0100

    basically completetly restartet as the plan wasnt going to work

--- Client/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (env) {
-  if (typeof env === 'undefined' || typeof env.devserver === 'undefined') {
-    return require('./webpack.config.dev');
-  } else if (typeof env.devserver !== 'undefined' && env.devserver) {
-    return require('./webpack.config.server');
-  } else if (typeof env.release !== 'undefined' && env.release) { 
-    return require('./webpack.config.release');
-  }
-}
