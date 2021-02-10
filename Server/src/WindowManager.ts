086ff801dd220675b0a5ec092d627ba3b58e1b0c
Torphexi <thiel1997@gmail.com>
2021-02-10 22:31:12 +0100

    Added basics for navigation System

--- Server/src/WindowManager.ts
+++ Server/src/WindowManager.ts
@@ -1,11 +1,16 @@
 import * as fs from 'fs';
 import * as path from "path";
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, ipcMain } from 'electron';
 
 
 export class WindowManager {
     private static instance: WindowManager;
 
+    private constructor(){
+        ipcMain.on('pageChange', (event , data) =>{
+            this.setContent(this.getPath(data));
+        });
+    }
     public Window: BrowserWindow;
 
     public static getInstance (): WindowManager {
@@ -24,5 +29,9 @@ export class WindowManager {
             }
             this.Window.webContents.send('update', data);
         });
+    }
+
+    private getPath(name: string):string{
+        return '../src/'+name+'/'+name+'.html'
     }
 }
--- Server/src/renderer.ts
+++ Server/src/renderer.ts
@@ -5,4 +5,16 @@ ipcRenderer.on('update', (event, data) => {
     element.innerHTML = data;
 });
 
+function onClick(){
+    console.log('click');
+}
 
+document.querySelector("body").addEventListener('click', function(e) {
+    var tar = e.target;
+    var anchor = (<HTMLElement>tar).closest('a');
+    if(anchor !== null) {
+      if(anchor.getAttribute('targetPage')){
+          ipcRenderer.send('pageChange', anchor.getAttribute('targetPage'))
+      }
+    }
+  }, false);
